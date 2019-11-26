682591c8a985b6a7b287f4f314014af08df17231
ep98847 <pena.elizabeth42@gmail.com>
2019-11-25 20:13:45 -0500

    sdfghjk

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 // JavaScript Document
 
+window.onload = init();
 
 function init() {
 
@@ -22,33 +23,53 @@ function init() {
         url: 'assets/data/menu.json',
         dataType: 'json',
         success: function (data) {
+
+            var menu = menuBuilder(data.menu);
+
+            $('nav').append(menu);
             
-            console.log('all good');
-            console.log(data.menu.length);
-            console.log(data.menu);
-
-            if (data.menu.length > 0) {
-
-                data.menu.forEach(function (data) {
-
-                    console.log(data.MenuName);
-                    console.log(data.MenuLink);
-
-                    $('nav').append('<a href="' + data.MenuLink + '">' + data.MenuName + '</a>');
-
-                });
-            }
+            $("#loaderDiv").fadeOut("slow");
 
         },
         error: function () {
+
             console.log('all is not good');
+
         }
     });
-
-
 
 }
 
 
+function menuBuilder(obj) {
 
-window.onload = init();
+    var theMenu = '';
+
+    if (obj.length > 0) {
+
+        theMenu = theMenu + '<ul>';
+
+        obj.forEach(function (item) {
+
+            theMenu = theMenu + '<li><a href="#">' + item.MenuName + '</a>';
+
+            if (item.Menus.length > 0) {
+
+                theMenu = theMenu + menuBuilder(item.Menus);
+
+            }
+
+            theMenu = theMenu + '</li>';
+
+        });
+
+        theMenu = theMenu + '</ul>';
+
+    } else {
+
+        console.log('no data');
+
+    }
+
+    return theMenu;
+}
